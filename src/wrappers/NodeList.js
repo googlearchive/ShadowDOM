d39b2639fc4aa3ab7e3db8568325b558d36bc075
Daniel Freedman <dfreedm@google.com>
2013-05-14 10:57:57 -0700

    fix attributions

--- src/wrappers/NodeList.js
+++ src/wrappers/NodeList.js
@@ -1,4 +1,4 @@
-// Copyright 2012 The Toolkitchen Authors. All rights reserved.
+// Copyright 2012 The Polymer Authors. All rights reserved.
 // Use of this source code is goverened by a BSD-style
 // license that can be found in the LICENSE file.
 
