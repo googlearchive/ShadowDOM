3bb9bcc9a4013a00d2c75871ffdb1ef1e517e1dd
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-09-03 03:49:11 +0200

    Fix for "TypeError: Argument 1 of Window.getComputedStyle does not implement interface Element."
    I got that error on canvas element, and the reason was that because of event bubbling event comes to `document` where this method fails.
    Did not tested for error `TypeError: Argument 1 of Window.getDefaultComputedStyle does not implement interface Element.` but it may fix it in some cases as well.

--- src/wrappers/Window.js
+++ src/wrappers/Window.js
@@ -59,6 +59,9 @@
   mixin(Window.prototype, {
     getComputedStyle: function(el, pseudo) {
       renderAllPending();
+      if (el instanceof HTMLDocument) {
+        return true;
+      }
       return originalGetComputedStyle.call(unwrap(this), unwrapIfNeeded(el),
                                            pseudo);
     },
